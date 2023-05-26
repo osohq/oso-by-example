4df1eaa101bf6e7d7a217795a045a92ab1b9d153
Mike Cen <uncommoncense@users.noreply.github.com>
2023-05-26 15:12:00 -0400

    Use client in node and python sample apps

--- getting-started/python/main.py
+++ getting-started/python/main.py
@@ -1,43 +1,14 @@
-import oso_sdk
-from fastapi import Depends, FastAPI, Request
-from oso_sdk.integrations.fastapi import FastApiIntegration
+from fastapi import FastAPI, HTTPException
+from oso_cloud import Oso
 
 # remember to update this before running the server!
-API_KEY = "<please provide your api key here>"
+api_key = "<please provide your api key here>"
+oso = Oso(url="https://cloud.osohq.com", api_key=api_key)
 
-oso = oso_sdk.init(
-    API_KEY,
-    FastApiIntegration(),
-    # only enforce routes with the `@oso.enforce` decorator
-    optin=True,
-)
-
-app = FastAPI(dependencies=[Depends(oso)])
-
-
-@oso.identify_user_from_request
-async def user(_request: Request) -> str:
-    """
-    Oso Cloud handles authorization - you'll need to handle your own
-    authentication. That means it's up to you to figure out *who* a user is
-    (for example, with a username and password).
-
-    For this sample application, we identify every user as "anonymous".
-
-    This means that the actor for requests is identified as `User{"anonymous"}`.
-    """
-    return "anonymous"
+app = FastAPI()
 
 
 @app.get("/org/{id}")
-@oso.enforce(
-    # actor
-    "{id}",
-    # action
-    "view",
-    # resource
-    "Organization",
-)
 async def get_organization(id: str):
     """
     This is the "view" endpoint for an Organization, keyed on `id`.
@@ -52,12 +23,17 @@ async def get_organization(id: str):
 
     Because the "viewer" role gives "view" permission, this will grant access.
     """
+    actor = {"type": "User", "id": "anonymous"}
+    resource = {"type": "Organization", "id": id}
+
+    if not oso.authorize(actor, "view", resource):
+        # Handle authorization failure
+        raise HTTPException(status_code=404)
 
     return {"id": id}
 
 
 @app.post("/org/{id}")
-@oso.enforce("{id}", "edit", "Organization")
 async def post_organization(id: str):
     """
     This is the "edit" endpoint for an Organization, keyed on `id`.
@@ -72,12 +48,17 @@ async def post_organization(id: str):
 
     Because the "owner" role gives "edit" permission, this will grant access.
     """
+    actor = {"type": "User", "id": "anonymous"}
+    resource = {"type": "Organization", "id": id}
+
+    if not oso.authorize(actor, "edit", resource):
+        # Handle authorization failure
+        raise HTTPException(status_code=404)
 
     return {"id": id}
 
 
 @app.get("/repo/{id}")
-@oso.enforce("{id}", "view", "Repository")
 async def get_repository(id: str):
     """
     This is the "view" endpoint for a Repository, keyed on `id`.
@@ -105,12 +86,17 @@ async def get_repository(id: str):
     Repository{"code"} (given the "repository_container" relation), this will grant
     access.
     """
+    actor = {"type": "User", "id": "anonymous"}
+    resource = {"type": "Repository", "id": id}
+
+    if not oso.authorize(actor, "view", resource):
+        # Handle authorization failure
+        raise HTTPException(status_code=404)
 
     return {"id": id}
 
 
 @app.post("/repo/{id}")
-@oso.enforce("{id}", "edit", "Repository")
 async def post_repository(id: str):
     """
     This is the "edit" endpoint for a Repository, keyed on `id`.
@@ -138,5 +124,11 @@ async def post_repository(id: str):
     on Repository{"code"} (given the "repository_container" relation), this will
     grant access.
     """
+    actor = {"type": "User", "id": "anonymous"}
+    resource = {"type": "Repository", "id": id}
+
+    if not oso.authorize(actor, "edit", resource):
+        # Handle authorization failure
+        raise HTTPException(status_code=404)
 
     return {"id": id}
